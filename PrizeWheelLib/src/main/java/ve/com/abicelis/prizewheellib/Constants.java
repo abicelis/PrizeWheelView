81e93e6aa2b117300a001182095ff682bfb3cbd7
abicelis <abicelis@gmail.com>
2017-07-26 13:48:30 -0400

    Constants for max / min wheel sections

---
+++ PrizeWheelLib/src/main/java/ve/com/abicelis/prizewheellib/Constants.java
@@ -0,0 +1,11 @@
+package ve.com.abicelis.prizewheellib;
+
+/**
+ * Created by abicelis on 26/7/2017.
+ */
+
+public class Constants {
+
+    public static final int MINIMUM_WHEEL_SECTIONS = 2;
+    public static final int MAXIMUM_WHEEL_SECTIONS = 10;
+}
