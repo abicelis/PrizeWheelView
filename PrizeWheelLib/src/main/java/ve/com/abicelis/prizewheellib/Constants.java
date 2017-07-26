7529df3e8a60d5dc5a90de264675d2457ccded77
abicelis <abicelis@gmail.com>
2017-07-26 18:45:01 -0400

    Refactored fling dampening constants

--- PrizeWheelLib/src/main/java/ve/com/abicelis/prizewheellib/Constants.java
+++ PrizeWheelLib/src/main/java/ve/com/abicelis/prizewheellib/Constants.java
@@ -5,7 +5,10 @@ package ve.com.abicelis.prizewheellib;
  */
 
 public class Constants {
-
     public static final int MINIMUM_WHEEL_SECTIONS = 2;
     public static final int MAXIMUM_WHEEL_SECTIONS = 12;
+
+    public static final float INITIAL_FLING_VELOCITY_DAMPENING = 3F;
+    public static final float FLING_VELOCITY_DAMPENING = 1.025F;
+
 }
