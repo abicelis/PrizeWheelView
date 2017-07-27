a39c5d90c05c515d75f80f2c71557e4485f8aca4
abicelis <abicelis@gmail.com>
2017-07-26 20:47:33 -0400

    Added a margin on the wheel  to fit marker

--- PrizeWheelLib/src/main/java/ve/com/abicelis/prizewheellib/Constants.java
+++ PrizeWheelLib/src/main/java/ve/com/abicelis/prizewheellib/Constants.java
@@ -8,6 +8,8 @@ public class Constants {
     public static final int MINIMUM_WHEEL_SECTIONS = 2;
     public static final int MAXIMUM_WHEEL_SECTIONS = 12;
 
+    public static final int WHEEL_MARGIN_FOR_MARKER_DP = 20;
+
     public static final float INITIAL_FLING_VELOCITY_DAMPENING = 3F;
     public static final float FLING_VELOCITY_DAMPENING = 1.025F;
 
