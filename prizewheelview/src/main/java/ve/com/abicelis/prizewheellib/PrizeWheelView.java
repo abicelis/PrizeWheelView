eae6547220b1f46e02d4ec78bf1ae499ba6b41ce
abicelis <abicelis@gmail.com>
2017-07-28 20:40:10 -0400

    Fixed tiny bug when dragging wheel. on drag x,y was taken from underlying relativelayout, which isnt always centered with rotating bitmap.. Which made some rotations look funny

--- prizewheelview/src/main/java/ve/com/abicelis/prizewheellib/PrizeWheelView.java
+++ prizewheelview/src/main/java/ve/com/abicelis/prizewheellib/PrizeWheelView.java
@@ -81,13 +81,11 @@ public class PrizeWheelView extends RelativeLayout {
     public PrizeWheelView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
-
     }
 
     public PrizeWheelView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs);
-
     }
 
     private void init(Context context, AttributeSet attrs) {
@@ -107,7 +105,7 @@ public class PrizeWheelView extends RelativeLayout {
         matrix = new Matrix();
         gestureDetector = new GestureDetector(context, new WheelGestureListener());
         touchListener = new WheelTouchListener();
-        setOnTouchListener(touchListener);
+        mWheel.setOnTouchListener(touchListener);
 
 
 
